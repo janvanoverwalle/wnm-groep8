3e5dba97e3bc6198ed596c994d5841f55b9e5934
Jan Van Overwalle <janvanoverwalle@gmail.com>
2016-09-30 15:58:13 +0200

    Edit to UserController

--- 3habits/src/AppBundle/Controller/UserController.php
+++ 3habits/src/AppBundle/Controller/UserController.php
@@ -5,17 +5,31 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
-class UserControllerController extends Controller
+use AppBundle\Entity\User;
+use AppBundle\Entity\Habit;
+use AppBundle\Entity\Weight;
+use AppBundle\Entity\Calorie;
+
+class UserController extends Controller
 {
     /**
-     * @Route("/habits/{user_id}", requirements={"user_id": "\d+"})
+     * @Route("/habits/{user_id}", requirements={"user_id": "\d+"}, defaults={"user_id"=0}))
      */
     public function showHabitsAction($user_id)
     {
+		$twig = 'AppBundle:UserController:show_habits.html.twig';
+		$success = false;
+		
+		if ($user_id == 0) {
+			return $this->render($twig, array(
+				"success" => $success
+			));
+		}
 		
 		
-        return $this->render('AppBundle:UserController:show_habits.html.twig', array(
-            // ...
+		
+        return $this->render($twig, array(
+            "success" => $success
         ));
     }
 
