e6d3d41b34c595f279a3d232cb7ec67323428b32
Jan Van Overwalle <janvanoverwalle@gmail.com>
2016-10-07 15:34:10 +0200

    Updates

--- 3habits/src/AppBundle/Controller/UserController.php
+++ 3habits/src/AppBundle/Controller/UserController.php
@@ -4,11 +4,15 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Response;
 
 use AppBundle\Entity\User;
 use AppBundle\Entity\Habit;
 use AppBundle\Entity\Weight;
 use AppBundle\Entity\Calorie;
+
+use AppBundle\Repository\UserRepository;
 
 class UserController extends Controller
 {
@@ -16,11 +20,9 @@ class UserController extends Controller
      * @Route("/habits/{user_id}", requirements={"user_id": "\d+"}, defaults={"user_id"=0}))
 	 * @Method("GET")
      */
-    public function showHabitsAction($user_id)
+    public function getHabitsAction($user_id)
     {
 		$twig = 'AppBundle:UserController:show_habits.html.twig';
-		$user = new User();
-		$habits = new Habit();
 		
 		if ($user_id == 0) {
 			return $this->render($twig, array(
@@ -28,7 +30,16 @@ class UserController extends Controller
 			));
 		}
 		
+		$em = $this->getDoctrine()->getManager();
+		$user = $em->getRepository('AppBundle:User')->find($user_id);
 		
+		if ($user == null) {
+			return $this->render($twig, array(
+				"success" => False
+			));
+		}
+		
+		$habits = $em->getRepository('AppBundle:User')->findAllHabitsById($user_id);
 		
         return $this->render($twig, array(
             "success" => True,
@@ -37,12 +48,16 @@ class UserController extends Controller
         ));
     }
 	
-	/**
+    /**
      * @Route("/habits/{user_id}", requirements={"user_id": "\d+"}, defaults={"user_id"=0}))
 	 * @Method("POST")
      */
-    public function showHabitsAction($user_id) {
+	public function postHabitsAction(Request $request) {
+		$id = $request->request->get('habit_id');
 		
+		return new Response(
+            '<html><body>id: '.$id.'</body></html>'
+        );
 	}
 
     /**
